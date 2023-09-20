11f8d78f4636e45f2ae93d6d5bb0efada031f09c
Bobby Doll <bdidds2@gmail.com>
2023-09-19 21:55:21 -0400

    Update nfl_values.R

--- nfl_values.R
+++ nfl_values.R
@@ -602,3 +602,8 @@ final_gt2 <- final_plays2 %>%
 ifelse(class(final_gt2) != "try-error",
        gtsave(final_gt2, expand = 100, filename = "NFL_Game_Values.png", vheight = 100, vwidth =1000),
        NA)
+
+
+# predictions -------------------------------------------------------------
+
+write_rds(standard_plays, file = paste0("NFL/", nfl_week, " - predictions.rds"))
