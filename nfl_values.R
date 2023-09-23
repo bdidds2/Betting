85ba600498a7bfd9199c9c9e181f5236cd63917e
Bobby Doll <bobbydoll11@gmail.com>
2023-09-23 18:44:22 -0400

    Update nfl_values.R

--- nfl_values.R
+++ nfl_values.R
@@ -566,7 +566,7 @@ nfl_game_gt <- nfl_game_data %>%
 
 
 ifelse(class(nfl_game_gt) != "try-error",
-       gtsave(nfl_game_gt, expand = 2000, filename = "NFL_Game_Values.png", vheight = 300, vwidth =1100),
+       gtsave(nfl_game_gt, expand = 1000, filename = "NFL_Game_Values.png", vheight = 300, vwidth =1700),
        NA)
 
 
